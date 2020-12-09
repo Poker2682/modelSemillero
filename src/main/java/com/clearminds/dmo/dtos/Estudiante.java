30bd7b7ea37f6ef44d7caff7305d39aa04284ef2
Daniel Montes <daniel.montes2682@gmail.com>
2020-12-09 10:18:34 -0500

    Version Inicial

---
+++ src/main/java/com/clearminds/dmo/dtos/Estudiante.java
@@ -0,0 +1,51 @@
+package com.clearminds.dmo.dtos;
+
+public class Estudiante {
+
+	private String nombre;
+	private String apellido;
+	private long id;
+
+	public Estudiante(String nombre, String apellido, long id) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.id = id;
+	}
+	
+	public Estudiante(String nombre, String apellido) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Estudiante [nombre=" + nombre + ", apellido=" + apellido + ", id=" + id + "]";
+	}
+
+}
